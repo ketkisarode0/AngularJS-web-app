4b482caa23d705b613233eacec0e71fcc592d93b
ketkisarode0 <ketki.sarode0@gmail.com>
2019-08-08 08:04:49 +0530

    8-8-19

--- scripts/mainscripts.js
+++ scripts/mainscripts.js
@@ -22,12 +22,17 @@ app.controller('bookController', ['$scope',function($scope){
     $scope.message = 'hello book';
 }]);
 
-app.controller('tableCtrl', function($scope){
+app.controller('tableCtrl', function($scope, $http){
         $scope.Books = [
             { BookId: 1, BookName: "Wealth of Nations", Author: "Adam Smith" },
             { BookId: 2, BookName: "Mudassar Khan", Author: "India" },
             { BookId: 3, BookName: "Suzanne Mathews", Author: "France" },
             { BookId: 4, BookName: "Robert Schidner", Author: "Russia" }
             ];
+
+            $http.get("http://localhost:3000/helloworld")
+            .then(function(response){
+                console.log(response.data.json);
+            });
 });
 
